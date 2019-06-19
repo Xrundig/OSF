87e8143eb444be16f375fcbacc6698b432ec002c
Xrundig <molokoiedovm@gmail.com>
2019-06-19 23:36:34 +0300

    middleOfJune

--- app/js/script.js
+++ app/js/script.js
@@ -7,17 +7,33 @@
 
 /*Запускаем карусель с учителями*/
 $(document).ready(function(){
-	
+
 	// Находим блок карусели
-	var carousel = $(".owl-carousel");
+	/*var heroCarousel = $(".hero-carousel");*/
 
 	// Запускаем плагин карусели
-	carousel.owlCarousel({
+	$('.carousel').slick({
+    dots: true,
+    arrows: true,
+    prevArrow: $('.prev'),
+    nextArrow: $('.next'),
+  });
+	
+	// Находим блок карусели
+	/*var carousel = $(".owl-carousel");*/
+
+	// Запускаем плагин карусели
+	$('.second').slick({
+	    slidesToShow: 3, 
+	  });
+	
+	/*$(".owl-carousel").owlCarousel({
 		loop:true,
     	margin:10,
     	nav:true,
     	navText: ["<div class='arrow-left'></div>, <div class='arrow-right'></div>"],
     	items: 4
-	});
+	});*/
 
 });
+
